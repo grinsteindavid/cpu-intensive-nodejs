c54461c08f9d410df5257872d0563130b9ef89f7
david miranda <davidmirandagrinstein@gmail.com>
2022-09-25 18:50:59 -0300

    update

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ function createChildProcess(payload) {
     x.send(payload);
     x.on("message", (data) => {
       console.log(`child process: ${data}`);
+      x.disconnect();
       resolve(data);
     });
     x.on("error", (msg) => {
+      x.disconnect();
       reject(`An error ocurred: ${msg}`);
     });
   });
@@ -17,24 +19,26 @@ function createChildProcess(payload) {
 
 function createWorker(payload) {
   return new Promise(function (resolve, reject) {
-    const worker = new Worker("./worker.js", {
-      workerData: { n: payload },
-    });
+    const worker = new Worker("./worker.js");
+    worker.postMessage(payload);
     worker.on("message", (data) => {
       console.log(`worker: ${data}`);
+      worker.terminate();
       resolve(data);
     });
     worker.on("error", (msg) => {
+      worker.terminate();
       reject(`An error ocurred: ${msg}`);
     });
   });
 }
 
-const main = async () => {
-  createChildProcess(50);
-  createChildProcess(50);
-  createChildProcess(50);
-  //   createWorker(50);
+const main = () => {
+  createWorker(5);
+  createChildProcess(6);
+  createChildProcess(6);
+  createChildProcess(6);
+  createWorker(9);
 };
 
 main();
--- worker.js
+++ worker.js
@@ -1,7 +1,9 @@
-const { workerData, parentPort } = require("worker_threads");
+const { parentPort } = require("worker_threads");
 
 const fibonacci = (n) => {
   return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-parentPort.postMessage(fibonacci(workerData.n));
+parentPort.on("message", (message) => {
+  parentPort.postMessage(fibonacci(message));
+});
